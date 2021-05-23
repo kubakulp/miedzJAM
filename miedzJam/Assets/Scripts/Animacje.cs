57e1e8439054891803882fd5b09c0566151b86c4
kubakulp <63971508+kubakulp@users.noreply.github.com>
2021-05-23 21:28:55 +0200

    zmiana sceny z intra

--- miedzJam/Assets/Scripts/Animacje.cs
+++ miedzJam/Assets/Scripts/Animacje.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Animacje : MonoBehaviour
 {
-
+    public void LoadNewScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
 }
